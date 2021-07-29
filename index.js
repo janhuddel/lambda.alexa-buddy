f99c0d4d7717d1675cf74e45f2623b55e6000316
Jan Rohwer <jan.rohwer@gmail.com>
2021-07-29 17:56:28 +0200

    obtain gateway-host from env-var

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const axios = require("axios");
 
+const GATEWAY = process.env.GATEWAY;
+
 exports.lambdaHandler = async (request, context) => {
   try {
-    const response = await axios.post(
-      "https://alexa-iobroker.rohwer.sh/api/v1/alexa-request",
-      {
-        request: request,
-        context: context,
-      }
-    );
+    const response = await axios.post(`${GATEWAY}/api/v1/alexa-request`, {
+      request: request,
+      context: context,
+    });
 
     return response.data;
   } catch (err) {
